34238dbf27f6db0f21481e5ff37670ec8c913406
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-01 19:54:47 +0200

    Added test case to see if instance became true when using the flying? method

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,6 +2,11 @@ require 'plane'
 
 describe Airliner do
 
-    it { is_expected. to respond_to :flying? }
+	it { is_expected. to respond_to :flying? }			# checks if the instance object responds to flying? method
+
+	plane = Airliner.new
+	it "it checks that the plane is flying" do			# checks if the instance object equals to true when calling flying? method
+		expect(plane.flying?).to eq true
+	end
 
 end
