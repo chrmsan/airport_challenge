1c2b696a6b9643185b03bbbe3778940b44d680ec
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-01 20:39:16 +0200

    Added test case for landing a plane

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,7 +2,16 @@ require 'airport'
 
 describe Airport do
 
+
   it { is_expected.to respond_to(:land).with(1).argument }
 
+
+  it "lands an airplane to an airport" do
+    array = []
+    plane = Airplane.new
+    array << plane
+
+    expect(subject.aprons).to eq array
+  end
 
 end
